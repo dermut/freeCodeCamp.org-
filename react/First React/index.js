56436210adfb1f42333209fc4caa794c12dcd63d
Jimin Kim <ultimate1994@naver.com>
2022-02-17 23:19:24 +0900

    first commit

---
+++ react/First React/index.js
@@ -0,0 +1,8 @@
+// Spend a minute memorizing the line below 
+ReactDOM.render(
+    <ul>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+    </ul>,
+ document.getElementById("root"));
