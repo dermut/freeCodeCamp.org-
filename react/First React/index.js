9030e156dd1dc65716ae927c81d801dbe7e60835
Jimin Kim <ultimate1994@naver.com>
2022-02-17 23:37:33 +0900

    JSX는 JS Object를 화면에 그대로 보이도록 해석하는 역할을 해준다.

--- react/First React/index.js
+++ react/First React/index.js
@@ -1,5 +1,20 @@
-let root = document.getElementById("root");
-let element = document.createElement("h1");
-element.textContent = "Hello, World!";
-element.className = "test";
-root.appendChild(element);
+const element = <h1 className="header">This is JSX</h1>
+console.log(element);
+
+/*
+콘솔로 찍어보면 자바스크립트 오브젝트인데, 화면으로는 HTML처럼 보임.
+{
+    "type": "h1",
+    "key": null,
+    "ref": null,
+    "props": {
+        "className": "header",
+        "children": "This is JSX"
+    },
+    "_owner": null,
+    "_store": {}
+}
+*/
+
+// JSX
+ReactDOM.render(element, document.getElementById("root"));
