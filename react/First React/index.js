0f889992ac1e8db2522cc3819e6ba9e7842fd448
Jimin Kim <ultimate1994@naver.com>
2022-02-17 23:43:56 +0900

    첫 번째 구현, navbar

--- react/First React/index.js
+++ react/First React/index.js
@@ -1,45 +1,12 @@
-// JSX, 최상단에 <div></div>로 감싸져 있음
-const page = (
-    <div>
-        <h1 className="header">This is JSX</h1>
-        <p>This is a paragraph</p>
-    </div>
+const nav = (
+    <nav>
+        <h1>Jimin, The Monster</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 )
-console.log(page);
 
-/*
-{
-    "type": "div",
-    "key": null,
-    "ref": null,
-    "props": {
-        "children": [
-            {
-                "type": "h1",
-                "key": null,
-                "ref": null,
-                "props": {
-                    "className": "header",
-                    "children": "This is JSX"
-                },
-                "_owner": null,
-                "_store": {}
-            },
-            {
-                "type": "p",
-                "key": null,
-                "ref": null,
-                "props": {
-                    "children": "This is a paragraph"
-                },
-                "_owner": null,
-                "_store": {}
-            }
-        ]
-    },
-    "_owner": null,
-    "_store": {}
-}
-*/
-
-ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(nav, document.getElementById("root"));
