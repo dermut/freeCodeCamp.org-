fbbcc0118acd3b6ca1707af99e9aaadbc3ee41b5
Jimin Kim <ultimate1994@naver.com>
2022-02-19 15:50:47 +0900

    create-react-app으로 새로운 리액트 앱 생성

--- react/First React/index.js
+++ react/First React/index.js
@@ -4,6 +4,25 @@ import ReactDOM from "react";
 function Page() {
     return (
         <div>
+            <Header />
+            <MainContent />
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                TEST
+            </nav>
+        </header>
+    );
+}
+
+function MainContent() {
+    return (
+        <div>
             <h1>Good Night</h1>
             <ol>
                 <li>One Night</li>
@@ -11,7 +30,7 @@ function Page() {
                 <li>Three Night</li>
             </ol>
         </div>
-    );
+    )
 }
 
-ReactDOM.render(<Page/>, document.getElementById("root"));
+ReactDOM.render(<Page />, document.getElementById("root"));
